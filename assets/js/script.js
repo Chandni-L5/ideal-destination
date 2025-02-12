e22ee23195fa5ba72cbe32462c7bce3dfca632d0
Chandni Shah <qssinhux@students.codeinstitute.net>
2025-02-12 13:42:01 +0000

    create the js question and answer variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,68 @@
-//Questions
-const questions = [{
-        "question": "What is your ideal type of holiday?",
-        "answer1": "Relaxing on the beach with a cocktail",
-        "answer1Total": "1",
-        "answer2": "Exploring a busy city and experiencing the culture",
-        "answer2Total": "2",
-        "answer3": "Adventure and a unique experience",
-        "answer3Total": "3",
+
+//Question and answer variables
+const quizQAndA = [
+    {
+        question1: "What is your ideal type of holiday?",
+        answers: {
+            "a": "Relaxing on the beach with a cocktail",
+            "b": "Exploring a busy city and experiencing the culture",
+            "c": "Adventure and a unique experience"
+        }
     },
     {
-        "question": "Where do you find yourself on a day off?",
-        "answer1": "Swimming",
-        "answer1Total": "1",
-        "answer2": "A spot of shopping",
-        "answer2Total": "2",
-        "answer3": "At the zoo",
-        "answer3Total": "3",
-    } {
-        "question": "You're at the restaurant, what are you ordering?",
-        "answer1": "Jerk Chicken",
-        "answer1Total": "1",
-        "answer2": "Sushi",
-        "answer2Total": "2",
-        "answer3": "Bbq Ribs",
-        "answer3Total": "3",
-    } {
-        "question": "Where do you like to stay when you are traveling?",
-        "answer1": "A luxurious resort",
-        "answer1Total": "1",
-        "answer2": "A traditional home/airbnb",
-        "answer2Total": "2",
-        "answer3": "A tent in the wilderness",
-        "answer3Total": "3",
-    } {
-        "question": "What is your go-to item to pack in your suitcase?",
-        "answer1": "Sunscreen and a swimsuit",
-        "answer1Total": "1",
-        "answer2": "Good walking shoes",
-        "answer2Total": "2",
-        "answer3": "A camera and bug spray",
-        "answer3Total": "3",
-    } {
-        "question": "What sounds ideal to you?",
-        "answer1": "Remote tranquility",
-        "answer1Total": "1",
-        "answer2": "Surrounded by lots of people",
-        "answer2Total": "2",
-        "answer3": "Surround by animals in nature",
-        "answer3Total": "3",
-    } {
-        "question": "If you close your eyes, what would you like to heal on your ideal holiday?",
-        "answer1": "Waves crashing on the beach",
-        "answer1Total": "1",
-        "answer2": "The hustle and bustle of a city",
-        "answer2Total": "2",
-        "answer3": "The sounds of wildlife",
-        "answer3Total": "3",
-    } {
-        "question": "What is your ideal mode of transportation?",
-        "answer1": "A boat or catamaran",
-        "answer1Total": "1",
-        "answer2": "A fast train or subway",
-        "answer2Total": "2",
-        "answer3": "A 4x4 jeep",
-        "answer3Total": "3",
+        question2: "Where do you find yourself on a day off?",  
+        answers: {
+            "a": "Swimming",
+            "b": "A spot of shopping",
+            "c": "At the zoo"
+        }   
+    },
+    {
+        question3: "You're at the restaurant, what are you ordering?",
+        answers: {
+            "a": "Jerk Chicken",
+            "b": "Sushi",
+            "c": "Bbq Ribs"
+        }
+    },
+    {
+        question4: "Where do you like to stay when you are traveling?",
+        answers: {
+            "a": "A luxurious resort",
+            "b": "A traditional home/airbnb",
+            "c": "A tent in the wilderness"
+        }
+    },
+    {
+        question5: "What is your go-to item to pack in your suitcase?",
+        answers: {
+            "a": "Sunscreen and a swimsuit",
+            "b": "Good walking shoes",
+            "c": "A camera and bug spray"
+        }
+    },
+    {
+        question6: "What sounds ideal to you?",
+        answers: {
+            "a": "Remote tranquility",
+            "b": "Surrounded by lots of people",
+            "c": "Surrounded by animals in nature"
+        }
+    },
+    {
+        question7: "If you close your eyes, what would you like to hear on your ideal holiday?",
+        answers: {
+            "a": "Waves crashing on the beach",
+            "b": "The hustle and bustle of a city",
+            "c": "The sounds of nature"
+        }
+    },
+    {
+        question8: "What is your ideal mode of transportation?",
+    answers: {
+        "a": "A boat or catamaran",
+        "b": "A fast train or subway",
+        "c": "A 4x4 jeep"
+        }
     }
 ]
-
-let currentQuestion = 0;
-let score = [];
-let selectedAnswersData = [];
-const totalQuestions = questions.length;
-
-const container = document.querySelector('.quiz-container');
-const questionElement = document.querySelector('.question');
-const opt1 = document.querySelector('.option1');
-const opt2 = document.querySelector('.option2');
-const opt3 = document.querySelector('.option3');
-const next = document.querySelector('.next-button');
-const previous = document.querySelector('.previous-button');
-const restart = document.querySelector('.restart');
