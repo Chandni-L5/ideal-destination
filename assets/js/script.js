0a199f9fe8defcd024a07a28927a3b36ddc78d22
Chandni Shah <qssinhux@students.codeinstitute.net>
2025-02-16 11:14:48 +0000

    Add the text content and styling to the q+a section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const startButton = document.querySelector('#start'); 
 // continueBtn 
 const quizSection = document.querySelector('#quiz-section');
+const submitButton = document.querySelector('#submit-button');
+// next button
 
 // Start button 
 startButton.onclick = () => {
